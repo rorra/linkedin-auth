de415243ed4e7624e9a2238ad173f150c29c00fc
Rodrigo Dominguez <rorra@rorra.com.ar>
2012-08-08 14:21:43 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LinkedinAuth::Application.config.secret_token = '4fff2602e741f654b83bb733db5cc6da74b634b10a1ef7baaf7886174b4a33efa2b754d8571b4008558c6dc326345504523575c25f8bb06a079a921afb2ee971'

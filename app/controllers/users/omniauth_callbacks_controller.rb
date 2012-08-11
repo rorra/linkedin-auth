8474bdd9a781e6305969c5c62c2e54a7970d7a00
Rodrigo Dominguez <rorra@rorra.com.ar>
2012-08-11 04:23:11 -0300

    Code refactoring

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -11,9 +11,8 @@ class Users::OmniauthCallbacksController < ApplicationController
         # If the user is the same, login, otherwise, warn that there is another user with that uid
         if User.where(linkedin_uid: omniauth['uid']).first.id == current_user.id
           # Update the credentials
-          current_user.linkedin_uid = omniauth['uid']
-          current_user.linkedin_token = credentials['token']
-          current_user.linkedin_secret = credentials['secret']
+          assign_linkedin_credentials(user, credentials, omniauth['uid'])
+
           current_user.save
 
           flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Linkedin"
@@ -32,16 +31,8 @@ class Users::OmniauthCallbacksController < ApplicationController
 
           data = client.profile(fields: %w(email-address first_name last_name headline industry picture-url public-profile-url))
 
-          current_user.linkedin_uid = omniauth['uid']
-          current_user.linkedin_token = credentials['token']
-          current_user.linkedin_secret = credentials['secret']
-
-          current_user.first_name = data['first_name']
-          current_user.last_name = data['last_name']
-          current_user.headline = data['headline']
-          current_user.industry = data['industry']
-          current_user.picture_url = data['picture_url']
-          current_user.public_profile_url = data['public_profile_url']
+          assign_linkedin_credentials(user, credentials, omniauth['uid'])
+          assign_linkedin_data(user, data)
 
           current_user.save
 
@@ -54,7 +45,7 @@ class Users::OmniauthCallbacksController < ApplicationController
       end
     else
       # The user is not logged in
-      # Check if there is a user with that uid, if it exists, login and redirect, otherwise fetch email and redirect
+      # Check if there is a user with that uid, if it exists, login and redirect, otherwise create the user
       if User.where(linkedin_uid: omniauth['uid']).exists?
         flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Linkedin"
         user = User.where(linkedin_uid: omniauth['uid']).first
@@ -68,19 +59,10 @@ class Users::OmniauthCallbacksController < ApplicationController
 
         user = User.new
 
-        user.linkedin_uid = omniauth['uid']
-        user.linkedin_token = credentials['token']
-        user.linkedin_secret = credentials['secret']
-
-        user.email = data['email_address']
-        user.first_name = data['first_name']
-        user.last_name = data['last_name']
-        user.headline = data['headline']
-        user.industry = data['industry']
-        user.picture_url = data['picture_url']
-        user.public_profile_url = data['public_profile_url']
-
+        assign_linkedin_credentials(user, credentials, omniauth['uid'])
+        assign_linkedin_data(user, data, true)
         user.password = user.password_confirmation = generate_password
+
         user.save
 
         flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Linkedin"
@@ -91,6 +73,24 @@ class Users::OmniauthCallbacksController < ApplicationController
   end
 
   private
+
+  def assign_linkedin_credentials(user, credentials, uid = nil)
+    user.linkedin_uid = uid unless uid.blank?
+    user.linkedin_token = credentials['token']
+    user.linkedin_secret = credentials['secret']
+    user
+  end
+
+  def assign_linkedin_data(user, data, email = false)
+    user.email = data['email_address'] if email
+    user.first_name = data['first_name']
+    user.last_name = data['last_name']
+    user.headline = data['headline']
+    user.industry = data['industry']
+    user.picture_url = data['picture_url']
+    user.public_profile_url = data['public_profile_url']
+    user
+  end
 
   def generate_password
     (0...8).map{65.+(rand(25)).chr}.join
